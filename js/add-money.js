a2335565cd20830efbf27699a4ce0f1e6823a83c
Zabir Arkam <zabirarkam27@gmail.com>
2024-12-03 21:34:39 +0600

    trying to make live link

--- js/add-money.js
+++ js/add-money.js
@@ -7,11 +7,28 @@ document.getElementById('btn-add-money').addEventListener('click', function (eve
 
     const newBalance = currentBalance + addAmount;
 
+    if(isNaN(addAmount)){
+        alert('Failed to add money.');
+        return;
+    }
+
     if (pinNumber === 1234) {
         document.getElementById('add-amount').value = '';
         document.getElementById('pin-number').value = '';
 
         document.getElementById('current-balance').innerText = newBalance;
+
+
+        // add to transaction History
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class = "bg-gray-300 mb-2 p-2 rounded-md">
+                <h4 class = "text-lg font-bold mb-2">Add Money</h4>
+                <p>Added: ${addAmount} Tk. New Balance: ${newBalance} Tk</p>
+            </div>
+        `
+        document.getElementById('transaction-container').appendChild(div);
+
     }
     else {
         alert('Please Enter Valid PIN.');
--- js/cash-out.js
+++ js/cash-out.js
@@ -8,11 +8,32 @@ document.getElementById('btn-cash-out').addEventListener('click', function (even
 
     const newBalance = currentBalance - cashOutAmount;
 
+    if(isNaN(cashOutAmount)){
+        alert('Failed to Cash Out');
+        return;
+    }
+
     if (pinNumber === 1234) {
         document.getElementById('cash-out').value = '';
         document.getElementById('pin-no').value = '';
 
+
+        if(cashOutAmount > currentBalance){
+            alert('You do not have enough money to cash out.');
+            return;
+        }
+
         document.getElementById('current-balance').innerText = newBalance;
+
+        // add to transaction History
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class = "bg-gray-300 mb-2 p-2 rounded-md">
+                <h4 class = "text-lg font-bold mb-2">Cash Out</h4>
+                <p>${cashOutAmount} Withdraw. New Balance: ${newBalance}</p>
+            </div>
+        `
+        document.getElementById('transaction-container').appendChild(div);
     }
     else {
         alert('Please Enter Valid PIN');
